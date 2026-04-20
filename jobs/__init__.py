3640b89421a7130d1bd3481728fea2c229894294
MichaelCarter999 <12.michael.carter@gmail.com>
2026-04-20 10:43:11 +0100

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,6 +1,6 @@
 from nautobot.apps.jobs import register_jobs
 from .hello_jobs import HelloJobs
-
+from .paramiko_jobs import ParamikoCommandRunner, ParamikoChangeVLAN, ParamikoChangeVLAN_by_Function
 
 register_jobs(
     HelloJobs
