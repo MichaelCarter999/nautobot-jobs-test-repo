c5e3bc9167592d022da66dca9a852ddaefe393e6
MichaelCarter999 <12.michael.carter@gmail.com>
2026-04-21 14:24:31 +0100

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -2,7 +2,7 @@ from nautobot.apps.jobs import register_jobs
 from .hello_jobs import HelloJobs
 from .paramiko_jobs import ParamikoCommandRunner, ParamikoChangeVLAN, ParamikoChangeVLAN_by_Function
 from .paramiko_jobs_v2 import ParamikoShowCommands, ParamikoChangeAccessVLAN, ParamikoChangeAccessVLANByObject
-from .port_bounce_job_button import PortBouncerButton
+# from .port_bounce_job_button import PortBouncerButton
 
 register_jobs(
     HelloJobs,
